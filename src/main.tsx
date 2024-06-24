bf3409bf171326b0cd450cc4c8d799cb8d7114f0
Adeleke Israel <izzyfresher@gmail.com>
2024-06-24 11:56:04 +0100

    fix: scroll to top #0

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./config/gateway.ts";
 import CookieBanner from "./components/Web/CookieBanner.tsx";
+import ScrollToTop from "./components/common/ScrollToTop.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+      <ScrollToTop/>
         <CookieBanner />
         <App />
       </BrowserRouter>
