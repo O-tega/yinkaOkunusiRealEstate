ae4f87ff6ae934d781b9684a7bab8fdcb758b020
O-tega <tega.myview@gmail.com>
2024-10-08 16:16:09 +0100

    feat: width update #3

--- src/components/Web/InfoText.tsx
+++ src/components/Web/InfoText.tsx
@@ -13,7 +13,7 @@ const InfoText: React.FC<TInfo> = ({ title, text, icon, count, id }) => {
   return (
     <>
       <motion.div
-        className={`space-y-3 px-5 py-2 rounded-lg hidden md:block text-center md:text-left ${
+        className={`space-y-3 px-5 pt-2 pb-5 rounded-lg hidden md:block text-center md:text-left ${
           count === id ? "bg-blue-100" : ""
         }`}
         animate={{
@@ -29,7 +29,7 @@ const InfoText: React.FC<TInfo> = ({ title, text, icon, count, id }) => {
           <p className={`md:text-[24px] text-[18px] font-[600] ${count === id ? "text-blue-800" : "text-black"}`}>
             {title}
           </p>
-          <p className="md:text-[18px] w-[90%] text-[12px] pt-2 font-medium">{text}</p>
+          <p className="md:text-[18px] w-full text-[12px] pt-2 font-medium">{text}</p>
         </div>
       </motion.div>
       <div className={`space-y-3 p-2 rounded-lg md:hidden text-center md:text-left `}>
